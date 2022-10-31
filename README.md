ca3d48eb5bd9bcc9bf4a148f86a2a2ccaf80bcc8
yjkim2 <yjkim2@foodtechkorea.com>
2022-10-31 18:21:28 +0900

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# carrot_market_flutter
+
+clone coding
+
+Reference: [개발하는 남자 당근마켓 클론코딩](https://www.youtube.com/watch?v=aYeBFDnPbkY&list=PLgRxBCVPaZ_3R0h7mCkLJ1RKh7XRvoZdF)
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:carrot_market_flutter/page/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white, foregroundColor: Colors.black),
+      ),
+      home: Home(),
+    );
+  }
+}
